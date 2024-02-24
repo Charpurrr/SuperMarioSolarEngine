3771a8efc700f2d659e876906b6717b87f547656
CharlotteaDev <66517745+Charpurrr@users.noreply.github.com>
2024-02-24 16:05:28 +0100

    Frame advancing (for debugging)

--- scenes/ui/ui.gd
+++ scenes/ui/ui.gd
@@ -5,6 +5,12 @@ extends CanvasLayer
 
 var bgm_muted: bool = false
 
+const FRAME_TIME: int = 2
+var frame_timer: int
+
+## Whether or not you can advance a frame.
+var can_fa: bool = false
+
 
 func _ready():
 	bgm_muted = LocalSettings.load_setting("Audio", "Music Muted", false)
@@ -14,17 +20,35 @@ func _ready():
 
 func _process(_delta):
 	music_control()
+	advance_frame()
 	resetting()
 	pausing()
 
+	if get_tree().paused == true and Input.is_action_just_pressed(&"frame_advance"):
+		can_fa = true
+
+
+## Frame advancing only works while paused.
+func advance_frame():
+	if not can_fa:
+		frame_timer = FRAME_TIME
+		return
+
+	get_tree().paused = false
+	frame_timer = max(frame_timer - 1, 0)
+
+	if frame_timer == 0:
+		get_tree().paused = true
+		can_fa = false
+
 
 func pausing():
-	if Input.is_action_just_pressed("pause"):
+	if Input.is_action_just_pressed(&"pause"):
 		get_tree().paused = !get_tree().paused
 
 
 func music_control():
-	if Input.is_action_just_pressed("mute"):
+	if Input.is_action_just_pressed(&"mute"):
 		bgm_muted = !bgm_muted
 
 		LocalSettings.change_setting("Audio", "Music Muted", bgm_muted)
@@ -36,5 +60,5 @@ func set_muted_bgm():
 
 
 func resetting():
-	if Input.is_action_just_pressed("reset"):
+	if Input.is_action_just_pressed(&"reset"):
 		get_tree().reload_current_scene()
