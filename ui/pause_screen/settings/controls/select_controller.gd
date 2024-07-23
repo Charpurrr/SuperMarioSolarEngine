124ff99598a186f4dbdb87acc5632d1e103b48d3
Charpurrr <yyoshidevv@gmail.com>
2024-07-23 23:47:11 +0200

    Controller Selector Detail

--- ui/pause_screen/settings/controls/select_controller.gd
+++ ui/pause_screen/settings/controls/select_controller.gd
@@ -2,8 +2,12 @@ extends OptionButton
 
 @export var options: ControlsContents
 
+var currently_disabled: bool
+
 
 func _ready():
+	_update_button()
+
 	Input.joy_connection_changed.connect(_update_list)
 
 
@@ -20,9 +24,28 @@ func _update_list(device_port: int, connected: bool):
 	else:
 		remove_item(get_item_index(device_port))
 
+	_update_button()
+
 	for id in Input.get_connected_joypads():
 		if saved_controller == Input.get_joy_name(id):
-			selected = id
+			selected = get_item_index(id)
+
+
+func _check_none_connected() -> bool:
+	return Input.get_connected_joypads().is_empty()
+
+
+func _update_button():
+	var none_connected: bool = _check_none_connected()
+
+	disabled = none_connected
+
+	if none_connected and not currently_disabled:
+		currently_disabled = true
+		add_item("No Controller(s) Detected", 0)
+	elif currently_disabled and not none_connected:
+		currently_disabled = false
+		remove_item(0)
 
 
 func _on_item_selected(index):
