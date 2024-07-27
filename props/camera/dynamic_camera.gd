7d43d806cf64c8beb49a77f4e6043b9ee072ccd5
Unequipped <77971199+Unequipped@users.noreply.github.com>
2024-07-27 10:05:10 +0800

    Re-adjustable offset (walking)
    Walking in the opposite direction to your offset camera causes the it to shift back to the centre aligning with the player.

--- props/camera/dynamic_camera.gd
+++ props/camera/dynamic_camera.gd
@@ -11,6 +11,7 @@ var default_offset: Vector2
 @export var max_x_shift: int = 50
 @export var max_y_shift: int = 15
 @export var x_shift_speed: float = 0.07
+@export var x_shift_speed_two: float = 0.01
 @export var y_shift_speed: float = 0.7
 
 @export var shift_trigger: float = 4 ## actor velocity threshold theat triggers shifting.
@@ -33,6 +34,8 @@ func shift_x():
 			offset.x = lerpf(offset.x, max_x_shift, x_shift_speed)
 		elif actor.movement.facing_direction == -1:
 			offset.x = lerpf(offset.x, -max_x_shift, x_shift_speed)
+	elif abs(actor.vel.x) < shift_trigger and sign(actor.movement.facing_direction) != sign(offset.x):
+		offset.x = lerpf(offset.x, 0, x_shift_speed_two)
 
 
 func shift_y():
