e542414e16e5957b865591d72c5eba05de5abd06
Unequipped <77971199+Unequipped@users.noreply.github.com>
2024-07-27 10:49:10 +0800

    Update dynamic_camera.gd

--- props/camera/dynamic_camera.gd
+++ props/camera/dynamic_camera.gd
@@ -13,10 +13,11 @@ var default_offset: Vector2
 @export var max_x_shift: int = 50
 @export var max_y_shift: int = 15
 @export var x_shift_speed: float = 0.07
-@export var x_shift_speed_two: float = 0.01
-@export var y_shift_speed: float = 0.7
+@export var x_reset_speed: float = 0.01
+@export var y_shift_speed: float = 0.2
+@export var y_reset_speed: float = 0.8
 
-@export var shift_trigger: float = 4 ## actor velocity threshold theat triggers shifting.
+@export var shift_trigger: float = 4 ## actor velocity threshold which triggers cam shifting.
 
 func _ready():
 	actor = get_parent() # completely unnecessary line 
@@ -36,17 +37,19 @@ func shift_x():
 			offset.x = lerpf(offset.x, max_x_shift, x_shift_speed)
 		elif actor.movement.facing_direction == -1:
 			offset.x = lerpf(offset.x, -max_x_shift, x_shift_speed)
+	
+	# walking in the opposite direction causes the camera to slowly shift back
 	elif abs(actor.vel.x) < shift_trigger and sign(actor.movement.facing_direction) != sign(offset.x):
-		offset.x = lerpf(offset.x, 0, x_shift_speed_two)
+		offset.x = lerpf(offset.x, default_offset.x, x_reset_speed)
 
 
 func shift_y(): # currntly works but needs to be rewritten.
 	if Input.is_action_pressed("up"): # and actor.vel == Vector2.ZERO: <- is this needed?
 		offset.y  = lerpf(offset.y, -max_y_shift, y_shift_speed)
-	elif Input.is_action_pressed("down"):
-		offset.y  = lerpf(offset.y, max_y_shift, y_shift_speed) # this line needs twaeking
+	#elif Input.is_action_pressed("down") and actor.vel == Vector2.ZERO: # requires frame counting or should be removed entirely
+		#offset.y  = lerpf(offset.y, max_y_shift, y_shift_speed) # this line needs tweaking
 	else:
-		offset.y = default_offset.y
+		offset.y = lerpf(offset.y, default_offset.y, y_reset_speed)
 
 
 func is_default_offset() -> bool: ## to check if the camera is at its default offset
