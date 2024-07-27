bd0f251109a5fe987fb21404ff7665a7c440abd7
Unequipped <77971199+Unequipped@users.noreply.github.com>
2024-07-27 09:21:31 +0800

    Update dynamic_camera.gd

--- props/camera/dynamic_camera.gd
+++ props/camera/dynamic_camera.gd
@@ -1,4 +1,4 @@
-class_name Lakitu
+class_name DynamicCamera # Lakitu
 extends Camera2D
 ## the word 'shift' refers to camera movement.
 ## Alternatively the word look can be used.
@@ -7,14 +7,13 @@ extends Camera2D
 
 var default_offset: Vector2 
 ## For resetting camera back to default offsets.
-## Accounts for manually adjusted camera positions.
-@export var max_x_shift: int = 30
+## Accounts for manually adjusted camera positions in editor.
+@export var max_x_shift: int = 50
 @export var max_y_shift: int = 15
 @export var x_shift_speed: float = 0.07
 @export var y_shift_speed: float = 0.7
 
-@export var shift_trigger: float = 4 #
-@export var look_up_frames: int = 30 ## How long the up key must be held before the camera begins to shift upwards 
+@export var shift_trigger: float = 4 ## actor velocity threshold theat triggers shifting.
 
 func _ready():
 	actor = get_parent() # completely unnecessary line 
@@ -30,19 +29,14 @@ func _process(delta):
 
 func shift_x():
 	if abs(actor.vel.x) > shift_trigger:
-		# you can't use input because if you're moving but not pressing dir it won't reg
-		# you can't use facing in case you're backwards sliding. Just going to use it for now though
 		if actor.movement.facing_direction == 1:
-			#offset.x = default_offset.x + max_x_shift
 			offset.x = accel_shift(offset.x, max_x_shift, x_shift_speed)
 		elif actor.movement.facing_direction == -1:
-			#offset.x = default_offset.x - max_x_shift
 			offset.x = accel_shift(offset.x, -max_x_shift, x_shift_speed)
 
 
 func shift_y():
-	if Input.is_action_pressed("up"):
-		#offset.y = default_offset.y - max_y_shift
+	if Input.is_action_pressed("up") and actor.vel == Vector2.ZERO:
 		offset.y  = accel_shift(offset.y, -max_y_shift, y_shift_speed)
 	else:
 		offset.y = default_offset.y	
@@ -54,5 +48,5 @@ func accel_shift(current_offset, target_offset, shift_speed):
 	return new_offset
 
 
-func look_up_counter():
-	pass
+func is_default_offset() -> bool: ## to check if the camera is at its default offset
+	return offset == default_offset
