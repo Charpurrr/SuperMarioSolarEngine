7df5e2eaa0700d718cbbaf58ea27ee3dc5b1c542
SilverCoin1719 <88743665+SilverCoin1719@users.noreply.github.com>
2024-08-15 23:40:04 -0400

    Add grass environment and mode 7 floor
    Grass environment uses legacy 127's hill background preview from the menu, a proper sprite in the right style and layout is probably needed for it to actually loop

---
+++ props/level_environments/bg_floor_mode_7.gd
@@ -0,0 +1,16 @@
+extends TextureRect
+
+@onready var environment: LevelEnvironment = get_parent()
+
+var cam_x : float
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	cam_x = -environment.camera.get_screen_center_position().x
+	
+	material.set_shader_parameter("mapPosition", Vector3(-cam_x/10000, -2, 0))
