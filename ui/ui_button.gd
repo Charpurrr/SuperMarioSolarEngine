6e9d7de0a69eca400c4c20024a9e87a55195bb4f
Charpurrr <yyoshidevv@gmail.com>
2025-02-18 12:10:28 +0100

    New Class: UI Button Options
    - Added a new type of UI button that toggles between different states (and saves them to LocalSettings if desired)

--- ui/ui_button.gd
+++ ui/ui_button.gd
@@ -1,4 +1,4 @@
-class_name PauseButton
+class_name UIButton
 extends Button
 ## A common UI button.
 
@@ -12,9 +12,12 @@ func _ready() -> void:
 ## The audio visual effects of a pause button.
 func avfx() -> void:
 	SFX.play_sfx(press_sfx, &"UI", self)
-
 	# Could optionally add visual effects too, I relied on the button themes instead.
 
 
 func _on_mouse_entered() -> void:
 	grab_focus()
+
+
+func _on_mouse_exited() -> void:
+	release_focus()
