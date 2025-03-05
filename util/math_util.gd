6e69dba5ec0200dcc3333b557079b6f422336368
SilverCoin1719 <88743665+SilverCoin1719@users.noreply.github.com>
2025-03-04 20:44:14 -0500

    camera motions now framerate independent
    - camera lookahead and zoom now do not rely on the framerate for their speed of modification
    - renamed a few variables in the camera script to better represent their purpose
    - removed roundp() and replaced any instances of it with snapped() instead
    - added lerp_weight_idp() which is used to make weight supplied to lerps that makes them framerate independent
    - enabled physics interpolation for testing purposes

--- util/math_util.gd
+++ util/math_util.gd
@@ -1,16 +1,13 @@
 class_name Math
-## Provides useful math functions that you can call using [code]Math.function_name[/code].
-
-
-## Round a value to a specific decimal place.
-static func roundp(value: float, decimal: int) -> float:
-	return round(value * pow(10, decimal)) / pow(10, decimal)
-
+## Provides useful math functions that you can call using [code]Math.function_name()[/code].
 
 ## If input is positive, returns 1. If input is negative or zero, returns 0.
 static func sign_positive(input: Variant) -> Variant:
 	return max(sign(input), 0)
 
+## Makes a weight supplied to a [code]lerp()[/code] function framerate independent (for procedural animations with [code]lerp()[/code]).
+static func lerp_weight_idp(weight : float, delta : float) -> float:
+	return 1-exp(-weight * delta)
 
 ### Returns the closest point on a line in relation to the reference point.
 #static func get_closest_point_line(
