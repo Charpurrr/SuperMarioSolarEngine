aab5fdd29c25a8059e5f12695b2ba4f20a31d17c
Charpurrr <yyoshidevv@gmail.com>
2025-03-09 00:02:09 +0100

    (ERR) Added Togglable Ticks to UISliders
    Theres a bug in Godot preventing this from properly working, so the code is commented out for now.
    
    https://github.com/godotengine/godot/issues/103839

--- ui/buttons/ui_slider.gd
+++ ui/buttons/ui_slider.gd
@@ -4,7 +4,16 @@ extends Control
 ## A common UI slider.[br]
 ## The script is a tool script so the visuals update accordingly.
 
-@export_range(0, 100) var default_value = 50
+# TODO: Add ticks, but there's a bug in Godot that prevents this.
+# ISSUE LINK: https://github.com/godotengine/godot/issues/103839
+
+@export_range(0, 100) var default_value: float = 50
+#@export var ticked: bool = false:
+	#set(val):
+		#ticked = val
+#
+		#if is_instance_valid(slider):
+			#_update_ticks()
 
 @export_category("References")
 @export var slider: HSlider
@@ -13,6 +22,7 @@ extends Control
 @export var grabber_button: Button
 @export var grabber_rect: ColorRect
 @export var progress: ProgressBar
+@export var tick_sound: AudioStream
 
 
 func _ready() -> void:
@@ -27,10 +37,10 @@ func _ready() -> void:
 
 
 func _update_size() -> void:
-	#progress.size.x = size.x
-	progress.size = size
-	slider.size.x = size.x
-	#grabber_button.size.y = size.y
+	progress.size.x = size.x
+	slider.size.x = size.x - 2
+
+	#_update_ticks()
 
 	grabber_path.curve.clear_points()
 	var x: float = size.x - grabber_button.size.x
@@ -39,6 +49,17 @@ func _update_size() -> void:
 	grabber_path.curve.add_point(Vector2(x, y))
 
 
+#func _update_ticks():
+	#if ticked == true:
+		#slider.tick_count = round(size.x / 8)
+	#else:
+		#slider.tick_count = 0
+
+
 func _update_slider(value: float) -> void:
 	progress.value = value
 	grabber_point.progress_ratio = value / 100
+
+	# If no sound effects are playing in the UI audio bus:
+	if get_tree().get_nodes_in_group(&"UI").is_empty():
+		SFX.play_sfx(tick_sound, &"UI", self)
