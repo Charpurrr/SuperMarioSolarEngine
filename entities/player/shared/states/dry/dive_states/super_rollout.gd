0e04127f72cea9f55fcb3f8a63eb4cded0c046d4
Charpurrr <yyoshidevv@gmail.com>
2025-03-19 21:28:53 +0100

    No More Infinite Sparkles During SDR
    it was kind of extreme lol

--- entities/player/shared/states/dry/dive_states/super_rollout.gd
+++ entities/player/shared/states/dry/dive_states/super_rollout.gd
@@ -3,6 +3,10 @@ extends DiveRollout
 ## Action based off of Legacy Super Mario 127's Super Dive Recover speedrun tech.
 ## Activated by diving and rolling out at the same time.
 
+## Number of sparkle particles that'll appear.
+const PARTICLE_AMT: int = 5
+var particle_counter: int
+
 ## The horizontal dive power added to your current velocity.
 @export var x_power: float = 6.0
 ## The maximum horizontal speed you need to reach before dives no longer
@@ -10,14 +14,17 @@ extends DiveRollout
 @export var speed_cap: float = 8.0
 
 
-
 func _on_enter(_param):
 	super(_param)
 
 	movement.accelerate(Vector2.RIGHT * movement.facing_direction * x_power, speed_cap)
 
+	particle_counter = PARTICLE_AMT
+
 
 func _subsequent_ticks():
 	super()
 
-	particles[1].emit_at(actor)
+	if particle_counter > 0:
+		particles[1].emit_at(actor)
+		particle_counter = max(particle_counter - 1, 0)
