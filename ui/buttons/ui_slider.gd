02eb87c1557a5f553499e331946e42aebabd3bc7
Davin <146634489+davin-christino@users.noreply.github.com>
2025-03-25 18:31:32 +0530

    UI slider features readded
    Included max value, slider step, and removed all z ordering.

--- ui/buttons/ui_slider.gd
+++ ui/buttons/ui_slider.gd
@@ -1,13 +1,46 @@
 @tool
 class_name UISlider
 extends Control
-## A common UI slider, with a range of 0 to 100.[br]
+## A common UI slider.[br]
 ## The script runs in the editor to update the visuals accordingly.
 
 # TODO: Add ticks, but there's a bug in Godot that prevents this.
 # ISSUE LINK: https://github.com/godotengine/godot/issues/103839
 
-@export_range(0, 100) var default_value: float = 50
+@export var max_value: float = 100.0:
+	set(val):
+		max_value = val
+
+		if is_instance_valid(slider):
+			slider.max_value = val
+			_update_slider(value, false)
+		if is_instance_valid(progress_bar):
+			progress_bar.max_value = val
+
+@export var value: float = 50.0:
+	set(val):
+		value = clampf(val, 0, max_value)
+		if is_instance_valid(slider):
+			slider.value = value
+		if is_instance_valid(progress_bar):
+			progress_bar.value = value
+		if is_instance_valid(grabber) and grabber.is_inside_tree():
+			grabber.set_anchor(SIDE_LEFT, value / max_value, true)
+			grabber.set_anchor(SIDE_RIGHT, value / max_value, true)
+		if is_instance_valid(outline) and outline.is_inside_tree():
+			outline.set_anchor(SIDE_LEFT, value / max_value, true)
+			outline.set_anchor(SIDE_RIGHT, value / max_value, true)
+
+@export var default_value: float = 50
+
+@export var slider_step: float = 1.0:
+	set(val):
+		slider_step = val
+
+		if is_instance_valid(slider):
+			slider.step = val
+
+
 #@export var ticked: bool = false:
 	#set(val):
 		#ticked = val
@@ -18,18 +51,12 @@ extends Control
 @export_category("References")
 @export var slider: HSlider
 @export var grabber: Control
+@export var outline: Control
 @export var progress_bar: ProgressBar
 @export var tick_sound: AudioStream
 
-var initialising: bool = false
-
 func _ready() -> void:
-	initialising = true
-
-	slider.value = default_value
-	_update_slider(default_value)
-
-	initialising = false
+	_update_slider(default_value, false)
 
 #func _update_ticks():
 	#if ticked == true:
@@ -37,20 +64,12 @@ func _ready() -> void:
 	#else:
 		#slider.tick_count = 0
 
-# This function is called when the slider's value changes,
-# setting the grabber and progress bar to match,
-# and playing the sound effect.
-func _update_slider(value: float) -> void:
-	# Update the progress bar.
-	progress_bar.value = value
-	
-	# Set the left and right anchors of the grabber,
-	# according to the value.
-	# This moves the grabber to the right location.
-	grabber.set_anchor(SIDE_LEFT, value / 100, true)
-	grabber.set_anchor(SIDE_RIGHT, value / 100, true)
+# This function is called when the slider is moved,
+# updating the visuals and optionally playing a sound effect.
+func _update_slider(new_value: float, play_sfx: bool) -> void:
+	value = new_value
 	
 	# If not playing on ready, and no sound effects are 
 	# playing in the UI audio bus:
-	if not initialising and get_tree().get_nodes_in_group(&"UI").is_empty():
+	if play_sfx and get_tree().get_nodes_in_group(&"UI").is_empty():
 		SFX.play_sfx(tick_sound, &"UI", self)
