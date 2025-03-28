2ca2de3762969c65ac1d10a1844840c6f5efd20b
Charpurrr <66517745+Charpurrr@users.noreply.github.com>
2025-03-29 00:31:14 +0100

    Multi-Monitor Layout Support (Window Resizing)

--- util/window_sizer.gd
+++ util/window_sizer.gd
@@ -14,7 +14,8 @@ static var last_none_fs: int = 1
 
 
 static func set_win_size(window_scale):
-	var screen_size: Vector2i = DisplayServer.screen_get_size()
+	var screen: int = DisplayServer.window_get_current_screen()
+	var screen_size: Vector2i = DisplayServer.screen_get_size(screen)
 	var window_size: Vector2i = default_size * (window_scale + 1)
 
 	# Set fullscreen if a window scale exceeds the bounds of the screen.
@@ -27,7 +28,20 @@ static func set_win_size(window_scale):
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 		DisplayServer.window_set_size(window_size)
 	
-		var window_pos: Vector2i = (screen_size - window_size) / 2
+		var screen_top_left: Vector2i = DisplayServer.screen_get_position(screen)
+
+		# On multi-monitor setups, the screen position is relative to the virtual desktop area.
+		# On multi-monitor setups with different screen resolutions or orientations,
+		# the origin may be located outside any display like this:
+		#
+		#* (0, 0)        +-------+
+		#                |       |
+		#+-------------+ |       |
+		#|             | |       |
+		#|             | |       |
+		#+-------------+ +-------+
+
+		var window_pos: Vector2i = screen_top_left + (screen_size - window_size) / 2
 		DisplayServer.window_set_position(window_pos)
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
