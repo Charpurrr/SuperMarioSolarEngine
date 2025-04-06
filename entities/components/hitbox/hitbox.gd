13367963096f7e07d8c0b92a05b71d787e21da13
Cruglet <cruglet@gmail.com>
2025-04-06 14:32:13 -0400

    Hitbox changes
    This is the major one
    There is now a `Hitbox` class that can be added to any CharacterBody2D.
    From there, there is an export variable called `damage_component` that defines the damage of the hitbox.
    - static functions like `can_harm_enemies` and `can_harm_players` are supplied to make checking those easier (from an Area2D)
    - `disabled` variable automatically toggles whether its collisonshape is enabled or not, so that getting the child is not necessary

---
+++ entities/components/hitbox/hitbox.gd
@@ -0,0 +1,23 @@
+class_name Hitbox extends Area2D
+
+@export var damage_component : DamageComponent
+
+var disabled: bool:
+	set(_disabled):
+		get_shape().set_deferred(&"disabled", _disabled)
+		disabled = _disabled
+
+func get_shape() -> CollisionShape2D:
+	return get_child(0) # better handling later perchance
+
+static func can_harm_enemies(area: Area2D):
+	return area is Hitbox \
+	&& area.is_in_group(&"enemy_harmable") \
+	&& area.damage_component \
+	&& !area.disabled
+
+static func can_harm_player(area: Area2D):
+	return area is Hitbox \
+	&& area.is_in_group(&"player_harmable") \
+	&& area.damage_component \
+	&& area.disabled
