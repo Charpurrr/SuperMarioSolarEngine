bda055daba5a2fa886877f5ee978ac7e81fcf579
Cruglet <cruglet@gmail.com>
2025-04-09 17:47:44 -0400

    Update goomba for enemy rework

--- entities/enemies/goomba/goomba.gd
+++ entities/enemies/goomba/goomba.gd
@@ -1,35 +1,7 @@
-class_name Goomba
-extends Enemy
-## Goomba behaviour.
+class_name Goomba extends Enemy
 
-@export var gravity: float = 1
+var target: Player
 
-@export var strike_x_power: float = 2
-@export var strike_y_power: float = 4
-
-## Whether or not the Goomba should die when ready.
-var ready_to_perish: bool = false
-
-
-func _physics_process(delta):
-	vel.y += gravity
-
-	# Die
-	if is_on_floor() and vel.y >= 0 and ready_to_perish:
-		vel.y = min(vel.y, 0)
-		queue_free()
-
+func _physics_process(delta: float) -> void:
+	vel.y += .3
 	super(delta)
-
-
-func die(source: Node, damage_type: HealthModule.DamageType):
-	match damage_type:
-		HealthModule.DamageType.STRIKE:
-			vel.x = source.vel.x - sign(source.position.x - position.x) * strike_x_power
-			vel.y = -strike_y_power
-
-			ready_to_perish = true
-			anime.play(&"die_strike")
-
-		HealthModule.DamageType.SQUISH:
-			pass
