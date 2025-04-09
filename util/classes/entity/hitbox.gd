44cd77663eda07f10837116b9444849dbcfc28c1
Cruglet <cruglet@gmail.com>
2025-04-09 16:11:34 -0400

    Hitbox class
    This class defines the behavior between two entity interactions. Right now, damage is the only thing being handled right now, but this can be expanded further if necessary.
    - Disabling the hitbox also disables all of its children.

---
+++ util/classes/entity/hitbox.gd
@@ -0,0 +1,18 @@
+class_name Hitbox extends Area2D
+
+@export var damage_component: DamageComponent
+
+@export var disabled: bool:
+	set(_disabled):
+		for child in get_children():
+			child.disabled = _disabled
+		disabled = _disabled
+
+signal damage_collision(damage: DamageComponent, sender: Area2D)
+
+func _ready() -> void:
+	area_entered.connect(_on_area_entered)
+
+func _on_area_entered(area: Area2D):
+	if area is Hitbox and area.damage_component and !area.disabled:
+		damage_collision.emit(area.damage_component, area)
