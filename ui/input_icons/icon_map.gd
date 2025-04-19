3b9e0f536f54f1b8902e7b34893b365de04da5bc
Cruglet <cruglet@gmail.com>
2025-04-19 15:52:58 -0400

    Update icon_map.gd

--- ui/input_icons/icon_map.gd
+++ ui/input_icons/icon_map.gd
@@ -4,7 +4,13 @@ extends Node
 
 
 static var icon_map: Resource = preload("res://ui/input_icons/icon_dictionary.tres")
+static var _event_map: Dictionary = {}
 
+static func _static_init() -> void:
+	if !_event_map:
+		for _event: InputEvent in icon_map.dictionary:
+			if _event != null:
+				_event_map.set(_event.as_text().hash(), _event)
 
 static func find(event: InputEvent) -> CompressedTexture2D:
-	return icon_map.dictionary.get(event, null)
+	return icon_map.dictionary.get(_event_map.get(event.as_text().hash()), null)
